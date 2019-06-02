28b6df9de30eb841560a6c71cc42c1871f3cd5f8
nozz_private <nozz_private@dn-mbp.local>
2019-06-02 21:20:03 +0900

    refactoring

--- get_cut_cmd.py
+++ get_cut_cmd.py
@@ -1,6 +1,7 @@
 import argparse
 from datetime import datetime, timedelta
 import os
+import pdb
 import time
 
 import cv2
@@ -31,9 +32,9 @@ while video.isOpened():
     if not ret:
         break
 
-    is_blackout = np.all(frame[50:600, 50:600, :] == 0)
+    is_bkout = np.all(frame[50:600, 50:600, :] == 0)
     
-    if is_blackout:
+    if is_bkout:
         bkout_frames.append(0)
         was_bkout = True
     else:
@@ -47,45 +48,43 @@ video.release()
 cv2.destroyAllWindows()
 
 bkout_frames = np.array(bkout_frames)
-bkout_time = np.where(bkout_frames==1)[0] / fps
+bkout_sec = np.where(bkout_frames==1)[0] / fps
 
-diffs = []
 games = []
-for i in range(len(bkout_time) - 1):
-    diff = bkout_time[i+1] - bkout_time[i]
-    if diff > 30:
-        games.append(np.array([bkout_time[i], bkout_time[i+1], diff]))
+for i in range(len(bkout_sec) - 1):
+    diff = bkout_sec[i+1] - bkout_sec[i]
+    if diff > 35:
+        games.append([bkout_sec[i], diff])
 
-last_extreme = len(bkout_frames) / fps - bkout_time[-1]
-if last_extreme > 30 and last_extreme < 400:
-    games.append(np.array([bkout_time[-1], len(bkout_frames) / fps, last_extreme]))
+margin_sec = len(bkout_frames) / fps - bkout_sec[-1]
+if margin_sec > 35 and margin_sec < 400:
+    games.append([bkout_sec[-1], margin_sec])
         
-games = np.array(games)
+timestamp = args.movie_path.split('/')[-1].split('.')[0]
+timestamp = datetime.strptime(timestamp, '%Y-%m-%d_%H-%M-%S')
 
-game_timestamps = []
 for game in games:
-    mins = np.uint8(game // 60)
-    secs = np.uint8(game % 60)
-    game_timestamp = ["{:02d}:{:02d}".format(_min, sec) for _min, sec in zip(mins, secs)]
-    game_timestamp[2] = game[0]
-    game_timestamp.append(game[2])
-    game_timestamps.append(game_timestamp)
+    mins = int(game[0] // 60)
+    secs = int(game[0] % 60)
+    delta = timedelta(minutes=mins, seconds=secs)
+    start_timestamp = timestamp + delta
+    start_timestamp = start_timestamp.strftime('%Y-%m-%d_%H-%M-%S')
+    game.append(start_timestamp)
+    
+    ss = round(int(game[0]))
+    duration = round(int(game[1]))
 
-ts = args.movie_path.split('/')[-1].split('.')[0]
-ts = datetime.strptime(ts, '%Y-%m-%d_%H-%M-%S')
-
-for gts in game_timestamps:
-    delta = timedelta(minutes=int(gts[0][:2]), seconds=int(gts[0][3:]))
-    stts = ts + delta
-    stts_str = stts.strftime('%Y-%m-%d_%H-%M-%S')
-    gts.append(stts_str)
-
-for gt in game_timestamps:
-    cmd = 'ffmpeg -ss {}  -i {} -t {} -vcodec copy -acodec copy {}.mp4'.format(int(round(gt[2])),
-                                                                               args.movie_path,
-                                                                               int(round(gt[3])),
-                                                                               os.path.join(args.output_dir, gt[4]))
+    output_path = os.path.join(args.output_dir, start_timestamp) + '.mp4'
+    
+    cmd = 'ffmpeg -ss {}  -i {} -t {} -vcodec copy -acodec copy {}'.format(ss,
+                                                                           args.movie_path,
+                                                                           duration,
+                                                                           output_path)
     print(cmd)
 
 elapsed_time = time.time() - start_time
 print('Elapsed time: {:.1f}min'.format(elapsed_time / 60))
+
+# save as csv
+df = pd.DataFrame(games, columns=['start_sec', 'duration', 'start_timestamp'])
+df.to_csv(os.path.join(args.output_dir, 'log.csv'))
